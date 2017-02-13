4174e8fdb3e5ef8bdeda62386ed5798d303b9a96
lary <l.ciminera@mycoachfootball.com>
2017-02-13 20:13:14 +0100

    regarde si je fou la merde

--- js/d3_radial_view.js
+++ js/d3_radial_view.js
@@ -1,13 +1,16 @@
-function radialMe(svg){
+function radialMe(toto){
 
-    svg.append('g')
+    toto.append('g')
         .append('text')
         .text("Coucou je suis du text svg de ma lib !")
         .attr("x", 20)
         .attr("y", 40)
         .style("text-anchor", "start")
         .attr("stroke", "#465754")
-         svg.append('g')
+
+
+
+      toto.append('g')
                 .append('text')
                 .text("fallais ecrire un truk j'ai dis")
                 .attr("x", 20)
