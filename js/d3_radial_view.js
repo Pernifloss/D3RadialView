9789047c52d728c625c049b828f0ead083e0147f
lary <l.ciminera@mycoachfootball.com>
2017-02-13 19:27:30 +0100

    add jsLib

---
+++ js/d3_radial_view.js
@@ -0,0 +1,11 @@
+function radialMe(svg){
+
+    svg.append('g')
+        .append('text')
+        .text("Coucou je suis du text svg de ma lib !")
+        .attr("x", 20)
+        .attr("y", 40)
+        .style("text-anchor", "start")
+        .attr("stroke", "#654754")
+    return detached;
+}
